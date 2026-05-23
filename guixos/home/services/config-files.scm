7a6ef9d74ee40f1a2a80e63d2d40fba416d9de9c
logoraz <erikalmaraz@fastmail.com>
2026-05-23 00:31:25 -0500

    refactor: inline dot-guile into config-files.scm
    Removes files/guile/.

--- guixos/home/services/config-files.scm
+++ guixos/home/services/config-files.scm
@@ -12,7 +12,23 @@
 ;;; Build-Time Assets
 ;;;
 (define %dot-guile
-  (resolve (config-source) "files/guile" #:file "dot-guile"))
+  (mixed-text-file
+   "dot-guile"
+   ";; -*- mode: scheme; -*-\n"
+   "\n"
+   ";; Use globally\n"
+   "(use-modules (ice-9 readline)\n"
+   "             (ice-9 format)\n"
+   "             (ice-9 pretty-print)\n"
+   "             (ice-9 colorized))\n"
+   "\n"
+   "(activate-readline)\n"
+   "(activate-colorized)\n"
+   "\n"
+   ";; G-Golf recommended duplicate binding handler\n"
+   "(use-modules (oop goops))\n"
+   "(default-duplicate-binding-handler\n"
+   "  '(merge-generics replace warn-override-core warn last))\n"))
 
 (define %podman-registries
   (mixed-text-file
@@ -27,6 +43,9 @@
    "# Use the short-name aliases table for common images\n"
    "short-name-mode = \"permissive\"\n"))
 
+;;;
+;;; Service Composition
+;;;
 (define (home-config-files-service config)
   `( ;; Guile Configuration
     (".guile" ,%dot-guile)
