d9b3cff9d62cdb0258a256e50c38ffdd9335c447
logoraz <erikalmaraz@fastmail.com>
2026-09-03 23:08:31 -0500

    feat (system): add adbusers group and android udev rules

--- guixos/system/channels.scm
+++ guixos/system/channels.scm
@@ -31,6 +31,6 @@
    (commit "13d0ca4948e87a6bcf04f6913cc5951ac6c12a60")
    (introduction
     (make-channel-introduction
-     "9edb3f66fd807b096b48283debdcddccfea34bad"
+     "c7362fcc1a7faeb4bfcb7c2fa97ebcaff149c99f"
      (openpgp-fingerprint
       "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
